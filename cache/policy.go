c2532d5c8a7f121884689b3d768d185957dde477
ayang-linux <3392406201@qq.com>
2023-04-12 15:10:16 +0800

    add:把 policy 和 ring 串起来

--- cache/policy.go
+++ cache/policy.go
@@ -1,10 +1,24 @@
 package cache
 
 type policy interface {
-	// DealGet 负责在缓冲区耗尽时批量接收和处理因为获取缓存导致键对应的 fre 需要更改等操作
-	DealGet([]uint64)
+	// consumer.ConsumeGet 负责在缓冲区耗尽时批量接收并发送到 policy 的接受 chan 中处理
+	consumer
 	// Add 把新的 key 加入缓存，如满足准入策略且缓存已满，则选择一部分需要淘汰的 key 返回
 	Add(uint64, int64) ([]*item, bool)
 	// Del 删除缓存
 	Del(uint64)
+}
+
+type defaultPolicy struct {
+	itemChan chan []uint64
+}
+
+func (policy *defaultPolicy) ConsumeGet(hashKeys []uint64) bool {
+	select {
+	case policy.itemChan <- hashKeys:
+		return true
+	default:
+		// 抗争用，如果缓冲区满了，不要阻塞直接丢弃
+		return false
+	}
 }
